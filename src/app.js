56ab68eee85c81fa3dcff4883eff7f2547ffca89
nolwenn <nolwenn.poilleux@gmail.com>
2020-02-04 20:03:17 +0100

    issue #8 : update generate dataset

--- src/app.js
+++ src/app.js
@@ -9,45 +9,91 @@ const fs = require('fs');
 
 const app = express();
 
+
 /** Configure network **/
 const hostname = '0.0.0.0';
 const port = 3000;
 
+
 /** New fixtures/dataset **/
 const fixtures = new Fixtures({
-    dir: './fixtures'
+    dir: './fixtures',
 });
 
 /** Database connection **/
 mongoose.connect('mongodb://mongo/' + process.env.DB_NAME);
 
-/** Load fixtures **/
-fixtures
-    .connect('mongodb://mongo/' + process.env.DB_NAME)
-    .then(() => fixtures.unload())
-    .then(() => fixtures.load())
-    .catch(e => console.error(e))
-    .finally(() => fixtures.disconnect());
 
-/** Generate dataset **/
-const user_list = [];
-const role_list = ['administrator', 'student', 'teacher'];
-for (let idx_user = 0; idx_user < 10; idx_user++) {
-    const random_user = {
-        first_name: faker.name.firstName(),
-        name: faker.name.lastName(),
-        pseudo: faker.internet.userName(),
-        password: faker.internet.password(),
-        role: role_list[Math.floor(Math.random() * role_list.length)]
-    };
-    user_list.push(random_user);
+const model_list = ['sessions', 'modules', 'users', 'marks'];
+const model_value = {};
+const prom_list = [];
+for (const model of model_list) {
+    model_value[model] = [];
+
+    /** Generate dataset **/
+    switch (model) {
+        case 'sessions':
+            // TO DO : random sessions here
+            break;
+        case 'modules':
+            // TO DO : random modules here
+            break;
+        case 'users':
+            // random users
+            const role_list = ['administrator', 'student', 'teacher'];
+            for (let idx_user = 0; idx_user < 10; idx_user++) {
+                const random_user = {
+                    first_name: faker.name.firstName(),
+                    name: faker.name.lastName(),
+                    pseudo: faker.internet.userName(),
+                    password: faker.internet.password(),
+                    role: role_list[Math.floor(Math.random() * role_list.length)],
+                };
+                model_value[model].push(random_user);
+            }
+            break;
+        case 'marks':
+            // TO DO : random marks here
+            break;
+        default:
+            console.log(`Nous ne générons pas de données pour "${model}".`);
+            break;
+    }
+
+    prom_list.push(
+        new Promise((resolve, reject) => {
+            // save data in json file
+            fs.writeFile(`./fixtures/${model}.json`, JSON.stringify(model_value[model], null, 2), (error) =>{
+                if (error) {
+                    reject(error);
+                }
+                else {
+                    resolve();
+                }
+            });
+        }),
+    );
 }
-// save users'list in json file
-fs.writeFileSync('./fixtures/users.json', JSON.stringify(user_list, null, 2));
+
+// Once promises resolved or rejected
+Promise.all(prom_list).then((error) => {
+    if (error) {
+        console.error(error);
+    }
+    /** Load fixtures once the json file is save **/
+    fixtures
+        .connect('mongodb://mongo/' + process.env.DB_NAME)
+        .then(() => fixtures.unload())
+        .then(() => fixtures.load())
+        .catch((error) => console.error(error))
+        .finally(() => fixtures.disconnect());
+});
+
 
 /** Replace special characters */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 /** Link to route files **/
 // Import anonymous functions in constant variables
@@ -60,6 +106,7 @@ const userRoute = require('./api/routes/userRoute');
 // moduleRoute(app);
 userRoute(app);
 // markRoute(app);
+
 
 /** Bind and listen for connections on the specified host and port **/
 app.listen(port, hostname);
