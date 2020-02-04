db43d00b7e6887e0086869f24b5cdc8c77fcc2c9
Ines <jebli1121@gmail.com>
2020-02-04 21:38:53 +0100

    Begin of CRUD modules

--- src/api/routes/moduleRoute.js
+++ src/api/routes/moduleRoute.js
@@ -1,7 +1,7 @@
 const moduleController = require('../controllers/moduleController');
 
 module.exports = (app) => {
-    app.route('/sessions/:session_id/modules')
+    app.route('/modules')
         .post(moduleController.create_a_module)
         .get(moduleController.list_all_modules);
         //.put(moduleController.update_a_module)
