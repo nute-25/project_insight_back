9ddc56386acde4bbab1f3ad48dc3b56cfea3d05f
Ines <jebli1121@gmail.com>
2020-02-07 11:33:53 +0100

    Continuation of CRUD Modules

--- src/api/controllers/moduleController.js
+++ src/api/controllers/moduleController.js
@@ -5,35 +5,98 @@ const Module = require('../models/moduleModel');
 exports.create_a_module = (req, res) => {
     const new_module = new Module(req.body);
     try {
-        new_module.save((error, user) =>{
+        new_module.save((error, module) =>{
             if (error) {
                 res.status(400);
-                console.log(error);
-                res.json({ message: 'Il manque des informations pour la création' });
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
             }
             else {
                 res.status(201);
-                res.json(modules);
+                res.json(module);
             }
         });
     }
-    catch (e) {
+    catch (error) {
         res.status((500));
-        console.log(e);
+        console.warn(error);
         res.json({ message: 'Erreur de serveur !!' });
     }
 };
 // READ
 exports.list_all_modules = (req, res) => {
-    Module.find({}, (error, modules) => {
+    Module.find({}, (error, module) => {
         if (error) {
             res.status(500);
-            console.log(error);
+            console.warn(error);
             res.json({ message: 'Erreur serveur !!' });
         }
         else {
             res.status(200);
-            res.json(modules);
+            res.json(module);
+        }
+    });
+};
+// UPDATE
+exports.update_a_module = (req, res) => {
+    try {
+        Module.findByIdAndUpdate(req.body._id, req.body, { new: true }, (error, module) => {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json(module);
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        console.warn(error);
+        res.json({ message: 'Erreur serveur !!' });
+    }
+};
+// DELETE
+exports.delete_a_module = (req, res) => {
+    try {
+        Module.findByIdAndRemove(req.body._id, (error) => {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json({ message: 'Le module a bien été supprimé' });
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        console.warn(error);
+        res.json({ message: 'Erreur serveur !!' });
+    }
+};
+// GET A MODULE
+exports.get_a_module = (req, res) => {
+    Module.findById(req.params.module_id, (error, module) => {
+        try {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json(module);
+            }
+        }
+        catch (error) {
+            res.status(500);
+            console.warn(error);
+            res.json({ message: 'Erreur serveur !!' });
         }
     });
 };
--- src/api/routes/moduleRoute.js
+++ src/api/routes/moduleRoute.js
@@ -3,11 +3,10 @@ const moduleController = require('../controllers/moduleController');
 module.exports = (app) => {
     app.route('/modules')
         .post(moduleController.create_a_module)
-        .get(moduleController.list_all_modules);
-        //.put(moduleController.update_a_module)
-        //.delete(moduleController.delete_a_module)
-  
-  //app.route('/modules/:module_id')
-      // .get(moduleController.get_a_module)
-  
-}
+        .get(moduleController.list_all_modules)
+        .put(moduleController.update_a_module)
+        .delete(moduleController.delete_a_module);
+
+    app.route('/modules/:module_id')
+        .get(moduleController.get_a_module);
+};
