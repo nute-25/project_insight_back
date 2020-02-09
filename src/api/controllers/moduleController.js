4501c50913102348e5fa7f2f59aad722f4eae921
Ines <jebli1121@gmail.com>
2020-02-09 09:02:51 +0100

    Udate CRUD Modules

--- src/api/controllers/moduleController.js
+++ src/api/controllers/moduleController.js
@@ -1,6 +1,111 @@
 /** Import librairy and user model **/
 const Module = require('../models/moduleModel');
+// si la session n'existe pas
+// CREATE
+exports.create_a_module = (req, res) => {
+    const new_module = new Module(req.body);
+    new_module.session_id = req.params.session_id;
+    try {
+        new_module.save((error, module) =>{
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(201);
+                res.json(module);
+            }
+        });
+    }
+    catch (error) {
+        res.status((500));
+        console.warn(error);
+        res.json({ message: 'Erreur de serveur !!' });
+    }
+};
+// UPDATE
+exports.update_a_module_from_a_session = (req, res) => {
+    module.session_id = req.params.session_id;
+    try {
+        Module.findByIdAndUpdate(req.body.module_id, req.body, { new: true }, (error, module) => {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json(module);
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        console.warn(error);
+        res.json({ message: 'Erreur serveur !!' });
+    }
+};
+// DELETE
+exports.delete_a_module_from_a_session = (req, res) => {
+    module.session_id = req.params.session_id;
+    try {
+        Module.findByIdAndRemove(req.body.module_id, (error) => {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json({ message: 'Le module a bien été supprimé' });
+            }
+        });
+    }
+    catch (error) {
+        res.status(500);
+        console.warn(error);
+        res.json({ message: 'Erreur serveur !!' });
+    }
+};
+// GET A MODULE FROM A SESSION
+exports.get_a_module_from_a_session = (req, res) => {
+    Module.findById(req.params.module_id, (error, module) => {
+        module.session_id = req.params.session_id;
+        try {
+            if (error) {
+                res.status(400);
+                console.warn(error);
+                res.json({ message: 'Il manque des informations' });
+            }
+            else {
+                res.status(200);
+                res.json(module);
+            }
+        }
+        catch (error) {
+            res.status(500);
+            console.warn(error);
+            res.json({ message: 'Erreur serveur !!' });
+        }
+    });
+};
+// READ
+exports.list_all_modules_from_a_session = (req, res) => {
+    Module.find({}, ({ session_id: req.params.session_id }, (error, module) => {
+        if (error) {
+            res.status(500);
+            console.warn(error);
+            res.json({ message: 'Erreur serveur !!' });
+        }
+        else {
+            res.status(200);
+            res.json(module);
+        }
+    }));
+};
 
+/*
 // CREATE
 exports.create_a_module = (req, res) => {
     const new_module = new Module(req.body);
@@ -100,3 +205,4 @@ exports.get_a_module = (req, res) => {
         }
     });
 };
+*/
--- src/api/routes/moduleRoute.js
+++ src/api/routes/moduleRoute.js
@@ -1,7 +1,16 @@
 const moduleController = require('../controllers/moduleController');
 
 module.exports = (app) => {
-    app.route('/modules')
+    app.route('sessions/session_id/module_id')
+        .post(moduleController.create_a_module)
+        .put(moduleController.update_a_module_from_a_session)
+        .delete(moduleController.delete_a_module_from_a_session);
+
+    app.route('sessions/session_id/modules')
+        .get(moduleController.list_all_modules_from_a_session);
+};
+/* {
+        app.route('/modules')
         .post(moduleController.create_a_module)
         .get(moduleController.list_all_modules)
         .put(moduleController.update_a_module)
@@ -10,3 +19,4 @@ module.exports = (app) => {
     app.route('/modules/:module_id')
         .get(moduleController.get_a_module);
 };
+*/
