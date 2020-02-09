5e64a6a3b3ffcba7e421f2399c7784dff4a976d6
nolwenn <nolwenn.poilleux@gmail.com>
2020-02-09 09:28:25 +0100

    issue #11: reduce access to page

--- src/api/routes/sessionRoute.js
+++ src/api/routes/sessionRoute.js
@@ -1,8 +1,10 @@
 const sessionController = require('../controllers/sessionController');
+const jwtMiddleware = require('../middleware/jwtMiddleware');
+
 
 module.exports = (app) => {
     app.route('/sessions')
-        .post(sessionController.create_a_session)
+        .post(/* jwtMiddleware.verify_token, */ sessionController.create_a_session)
         .get(sessionController.list_all_sessions)
         .put(sessionController.update_a_session)
         .delete(sessionController.delete_a_session);
