4ea905350f05df2e578119316293a3612d8720c5
nolwenn <nolwenn.poilleux@gmail.com>
2020-02-10 01:30:53 +0100

    issue #33 : modify get average per module

--- src/api/controllers/markController.js
+++ src/api/controllers/markController.js
@@ -43,56 +43,38 @@ exports.register_a_mark = (req, res) => {
 
 
 exports.get_average_mark_from_a_module = (req, res) => {
-    const { module_id } = req.params;
-
     try {
-        Module.findById(module_id, (error, module) => {
-            if (error) {
-                res.status(400);
-                console.warn(error);
-                res.json({ message: 'Module introuvable' });
-            }
-            else {
-                // the module exists
-                // console.log(`module_id : ${module_id} existe, enregistrement de la note`);
-
-                Mark.find({ module_id/* : module_id */ }, (error, marks) => {
-                    if (error) {
-                        res.status(400);
-                        console.warn(error);
-                        res.json({ message: 'Votre demande n\'a pu aboutir' });
-                    }
-                    else {
-                        res.status(200);
-                        // return marks
-                        res.json(marks);
-                        // display mark
-                        // for (let index = 0; index < marks.length; index ++) {
-                        //     console.log(index, marks[index]);
-                        //     console.log('note', marks[index].mark);
-                        // }
-
-                        // marks.aggregate(
-                        //     [
-                        //         {
-                        //             $group: {
-                        //                 _id: '$module_id',
-                        //                 avgMark: { $avg: '$mark' },
-                        //             },
-                        //         },
-                        //     ], (aggregate_error, aggregate_result) => {
-                        //         if (aggregate_error) {
-                        //             res.json({ aggregate_error });
-                        //         }
-                        //         else {
-                        //             res.json({ aggregate_result });
-                        //         }
-                        //     },
-                        // );
-                    }
-                });
-            }
-        });
+        Mark.aggregate(
+            [
+                {
+                    $group: {
+                        _id: '$module_id',
+                        avgMark: { $avg: '$mark' },
+                    },
+                },
+            ], (aggregate_error, aggregate_result) => {
+                if (aggregate_error) {
+                    res.status(400);
+                    console.warn(error);
+                    res.json({ aggregate_error });
+                }
+                else {
+                    // Module.findById(aggregate_result._id, (error, module) => {
+                    //     if (error) {
+                    //         res.status(400);
+                    //         console.warn(error);
+                    //         res.json({ message: 'Module introuvable' });
+                    //     }
+                    //     else {
+                    //         res.status(200);
+                    //         // start_date, end_date, speaker_id => first_name and name of user
+                    //     }
+                    // });
+                    res.status(200);
+                    res.json({ aggregate_result });
+                }
+            },
+        );
     }
     catch (error) {
         // le serveur n'a su exécuter la requête du client
--- src/api/routes/markRoute.js
+++ src/api/routes/markRoute.js
@@ -2,6 +2,8 @@ const markController = require('../controllers/markController');
 
 module.exports = (app) => {
     app.route('/modules/:module_id/marks')
-        .post(markController.register_a_mark)
+        .post(markController.register_a_mark);
+
+    app.route('/marks/average')
         .get(markController.get_average_mark_from_a_module);
 };
